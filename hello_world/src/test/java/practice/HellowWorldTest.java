5b6f20f88de87dfe7238d69b016a0b134e4a0ec0
Matt-Zac <mrz3927@g.rit.edu>
2026-01-21 13:51:41 -0500

    Created a simple Hello World file

---
+++ hello_world/src/test/java/practice/HellowWorldTest.java
@@ -0,0 +1,19 @@
+package practice;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class HellowWorldTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
